82312a22bc02c8fb41a6e0d71dbf60fb5583b9db
kawamataryou <ba068082@gmail.com>
2020-04-19 21:18:19 +0900

    [from now] 2020/04/19 21:18:19
    diff --git a/app/graphql/queries/tasks.rb b/app/graphql/queries/tasks.rb
    new file mode 100644
    index 0000000..46feb61
    --- /dev/null
    +++ b/app/graphql/queries/tasks.rb
    @@ -0,0 +1,15 @@
    +# frozen_string_literal: true
    +
    +module Queries
    +  module Tasks
    +    extend ActiveSupport::Concern
    +
    +    included do
    +      field :tasks, ::Types::TaskType, null: false
    +    end
    +
    +    def tasks
    +      Task.all
    +    end
    +  end
    +end
    diff --git a/app/graphql/types/query_type.rb b/app/graphql/types/query_type.rb
    index 46fa7de..ce26ba3 100644
    --- a/app/graphql/types/query_type.rb
    +++ b/app/graphql/types/query_type.rb
    @@ -1,13 +1,5 @@
     module Types
       class QueryType < Types::BaseObject
    -    # Add root-level fields here.
    -    # They will be entry points for queries on your schema.
    -
    -    # TODO: remove me
    -    field :test_field, String, null: false,
    -      description: "An example field added by the generator"
    -    def test_field
    -      "Hello World!"
    -    end
    +    include Queries::Tasks
       end
     end

---
+++ app/graphql/queries/tasks.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Queries
+  module Tasks
+    extend ActiveSupport::Concern
+
+    included do
+      field :tasks, ::Types::TaskType, null: false
+    end
+
+    def tasks
+      Task.all
+    end
+  end
+end
